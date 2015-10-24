e7b8f8ecfa9cf95ed7f1b51ed9c2cefbf1ba5fa2
tbragaf <braga.tiago.93@gmail.com>
2015-10-24 13:22:26 +0100

    Add index and bootstrap

---
+++ src/bootstrap.ts
@@ -0,0 +1,8 @@
+// import {bind, bootstrap} from 'angular2/angular2';
+// import {ROUTER_BINDINGS, ROUTER_PRIMARY_COMPONENT} from 'angular2/router';
+// import {App} from './components/app/app';
+// 
+// bootstrap(App, [
+//   ROUTER_BINDINGS,
+//   bind(ROUTER_PRIMARY_COMPONENT).toValue(AppCmp)
+// ]);
