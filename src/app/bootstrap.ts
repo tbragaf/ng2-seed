4b2c660348a85e526621924c3b9a6e6130baca1b
tiago <tiago.braga@wintouch.pt>
2015-11-26 15:24:40 +0000

    Remove unnecessary components

--- src/app/bootstrap.ts
+++
@@ -1,8 +0,0 @@
-import {provide, bootstrap} from 'angular2/angular2';
-import {ROUTER_PROVIDERS, HashLocationStrategy, LocationStrategy} from 'angular2/router';
-import {App} from './app';
-
-bootstrap(App, [
-	ROUTER_PROVIDERS,
-	provide(LocationStrategy, {useClass: HashLocationStrategy})
-]);
